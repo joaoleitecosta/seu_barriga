41cac914ff5c7da59b5fd34e65b0d606ae21aaec
Joao Leite <joao_leite@hotmail.com.br>
2020-04-19 20:14:21 -0300

    Configurando log de consultas no banco para testes

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
 const knexfile = require('../knexfile');
+//const knexLogger = require('knex-logger');
 
 // TODO criar chaveamento dinamico
 app.db = knex(knexfile.test);
+//app.use(knexLogger(app.db));
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
@@ -16,5 +18,19 @@ consign({ cwd: 'src', verbose: false })
 app.get('/', (req, res) => {
   res.status(200).send();
 });
+
+app.db
+  .on('query', (query) => {
+    console.log({
+      sql: query.sql,
+      bindings: query.bindings ? query.bindings.join(',') : '',
+    });
+  })
+  .on('query-response', (response) => {
+    console.log(response);
+  })
+  .on('error', (error) => {
+    console.log(error);
+  });
 
 module.exports = app;
